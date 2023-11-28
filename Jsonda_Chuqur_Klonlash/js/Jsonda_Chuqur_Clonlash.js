240ed3d216e1391c8a0e1d9fe6b4650474ba4eb7
umar5828 <jorayevu30@gmail.com>
2023-11-28 16:48:56 +0500

    S

---
+++ Jsonda_Chuqur_Klonlash/js/Jsonda_Chuqur_Clonlash.js
@@ -0,0 +1,13 @@
+"use strict";
+const car = {
+  name: "BMW",
+  model: "X5",
+  color: "Black",
+};
+// JSON.stringify() metodi obyektni JSON formatga o'giradi
+// const objToJSON = JSON.stringify(car);
+// console.log(objToJSON);
+
+// JSON.parse() metodi JSON formatdagi obyektni o'zgaruvchiga o'giradi
+// const JSONtoObj = JSON.parse(objToJSON);
+// console.log(JSONtoObj);
